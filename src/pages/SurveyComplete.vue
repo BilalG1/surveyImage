95f838bd97d06d74933e4931f0f702977b872110
Bilal Godil <bilal1@PersonalMicrosoftSoftware.ucla.edu>
2022-04-26 14:34:34 -0700

    html2canvas

--- src/pages/SurveyComplete.vue
+++ src/pages/SurveyComplete.vue
@@ -1,19 +1,22 @@
 <template>
-	<div style="height: 100%">
-		<img 
-			id="img-main"
-			src="@/assets/cert2Cleared.jpeg" 
-		/>
-	</div>
-	<div id="date" class="center-x">
-		{{getFormattedDate()}}
-	</div>
-	<div id="uclaId" class="center-x">
-		UCLA ID:&nbsp;{{uid}}
+	<div id="full-img">
+		<div style="height: 100%">
+			<img 
+				id="img-main"
+				src="@/assets/cert2Cleared.jpeg" 
+			/>
+		</div>
+		<div id="date" class="center-x">
+			{{getFormattedDate()}}
+		</div>
+		<div id="uclaId" class="center-x">
+			UCLA ID:&nbsp;{{uid}}
+		</div>
 	</div>
 </template>
 
 <script>
+import html2canvas from 'html2canvas'
 export default {
 	props: ["uid"],
 	methods: {
@@ -23,11 +26,34 @@ export default {
 			const formattedDay = (d.getDate() > 9) ? d.getDate() : '0'+(d.getDate())
 			return formattedMonth + '/' + formattedDay + '/' + new Date().getFullYear() 
 		}
+	},
+	async mounted() {
+		// await new Promise(r => setTimeout(r,10000))
+		const imgFilled = document.getElementById('full-img')
+		html2canvas(imgFilled)
+		.then(canvas => {
+            canvas.style.display = 'none'
+            document.body.appendChild(canvas)
+            return canvas
+        })
+        .then(canvas => {
+            const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+            const a = document.createElement('a')
+            a.setAttribute('download', 'my-image.png')
+            a.setAttribute('href', image)
+            a.click()
+            canvas.remove()
+        })
 	}
 }
 </script>
 
 <style scoped>
+#full-img{
+	height: 100%;
+	width: fit-content;
+	margin: 0 auto;
+}
 #img-main{
 	height: 100%;
 	position: relative;
